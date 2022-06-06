6b9b71e0978deeeb01cf8d646fe910035929a257
reynaldoperalta <reynaldopema14@gmail.com>
2022-06-05 21:42:42 -0500

    laboratorio 12

---
+++ lab12/Controllers/PersonaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab12.Models;
+
+namespace lab12.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Juan",
+                Apellido = "Peres",
+                Direccion = "Av Evergreen 123",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@gmail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av Progreso 325",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@gmail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av Los manzanos 101",
+                FechaNac = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@gmail.com"
+            });
+            return View(personas);
+        }
+    }
+}
---
+++ lab12/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab12.Models
+{
+    public class Persona
+    {
+        public int PersonaID { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public DateTime FechaNac { get; set; }
+        public String Direccion { get; set; }
+        public String Email { get; set; }
+    }
+}
