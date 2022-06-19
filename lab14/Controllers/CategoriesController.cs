f86b61609d351a2d35d28659a7bd269d945cf29a
reynaldoperalta <reynaldopema14@gmail.com>
2022-06-19 17:11:52 -0500

    laboratorio 14

---
+++ lab14/Controllers/CategoriesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab14.Models;
+using System.Net;
+
+namespace lab14.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        private NorthwindEntities _contexto;
+
+        public NorthwindEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new NorthwindEntities();
+                return _contexto;
+            }
+        }
+        public ActionResult Index()
+        {
+            return View(Contexto.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int id)
+        {
+            var productosPorcategoria = from p in Contexto.Products
+                                        orderby p.ProductName ascending
+                                        where p.CategoryID == id
+                                        select p;
+            return View(productosPorcategoria.ToList());
+        }
+
+        // GET: /Categories/Create
+        //mostrar formulario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories nuevaCategoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Categories.Add(nuevaCategoria);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                return View(nuevaCategoria);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Categories categoriaEditar = Contexto.Categories.Find(id);
+
+            if (categoriaEditar == null)
+                return HttpNotFound();
+
+            return View(categoriaEditar);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categories CategoriaEditar)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Entry(CategoriaEditar).State = System.Data.Entity.EntityState.Modified;
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                return View(CategoriaEditar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Categories categoriaEliminar = Contexto.Categories.Find(id);
+
+            if (categoriaEliminar == null)
+                return HttpNotFound();
+
+            Contexto.Categories.Remove(categoriaEliminar);
+            Contexto.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id, Categories Categoria1)
+        {
+            try
+            {
+                Categories categoriaEliminar = new Categories();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                    categoriaEliminar = Contexto.Categories.Find(id);
+
+                    if (categoriaEliminar == null)
+                        return HttpNotFound();
+
+                    Contexto.Categories.Remove(categoriaEliminar);
+                    Contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                return View(categoriaEliminar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
